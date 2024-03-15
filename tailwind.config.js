64b4a8ae646b7dbc85c6ddadd975ea9680a202de
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-03-14 22:41:57 -0600

    Replace skills sections with projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,20 @@ module.exports = {
     borderWidth: {
       1: "1px",
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        "bg-shift": {
+          "0%": { backgroundPosition: "0 0" },
+          "100%": { backgroundPosition: "100% 0" },
+        },
+      },
+      animation: {
+        "bg-shift": "bg-shift 1.5s linear infinite",
+      },
+      backgroundImage: {
+        rainbow: `linear-gradient(to right, darkorange, yellow, darkorange, yellow)`,
+      },
+    },
   },
   plugins: [],
 };
